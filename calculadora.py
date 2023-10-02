90d01888b80b5bbab5f8c7fd6ca99ed0f13abdea
tai <146776057+taitai237@users.noreply.github.com>
2023-10-02 17:13:01 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+#Solicita números ao usuário 
+num1 = float(input("Digite o primeiro número:"))
+num2 = float(input("Digite o segundo número"))
+
+#Realiza operações 
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 - num2
+divisao = num1 / num2 
+
+#Exibe os resultados 
+print(f"soma:{soma}")
+print(f"subtração:{subtracao}")
+print(f"multiplicação:{multiplicacao}")
+print(f"divisão:{divisao}")
---
+++ soma de dois números.py
@@ -0,0 +1,9 @@
+#Solicita números ao usuário
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+
+#Realiza operações 
+soma = num1 + num2
+
+#Exibe os resultados 
+print(f"soma:{soma}")
